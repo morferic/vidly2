d46dec732015c40023b5588c8a758223a5cba3ff
Eric Ranieri <eric.ranieri@hotmail.com>
2018-12-28 13:17:48 +0100

    Creato database

--- Vidly2/Migrations/201812281202400_InitialModel.cs
+++ Vidly2/Migrations/201812281202400_InitialModel.cs
@@ -8,6 +8,15 @@ namespace Vidly2.Migrations
         public override void Up()
         {
             CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
                 "dbo.AspNetRoles",
                 c => new
                     {
@@ -94,6 +103,7 @@ namespace Vidly2.Migrations
             DropTable("dbo.AspNetUsers");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Customers");
         }
     }
 }
